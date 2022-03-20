b1cc96bdaeec126ea127872c28a6f53df0c2d70b
Daniel Barlow <dan@telent.net>
2022-03-20 14:06:03 +0000

    add fwupd so we can have community modem firmware

--- hw-module.nix
+++ hw-module.nix
@@ -16,6 +16,25 @@
     hardware.sensor.iio.enable = true;
     hardware.firmware = [ config.mobile.device.firmware ];
 
+    services.fwupd = {
+      enable = true;
+    };
+
+    environment.etc."fwupd/remotes.d/testing.conf" = {
+      mode = "0644";
+      text = ''
+        [fwupd Remote]
+
+        Enabled=true
+        Title=Linux Vendor Firmware Service (testing)
+        MetadataURI=https://cdn.fwupd.org/downloads/firmware-testing.xml.gz
+        ReportURI=https://fwupd.org/lvfs/firmware/report
+        OrderBefore=lvfs,fwupd
+        AutomaticReports=false
+        ApprovalRequired=false
+      '';
+    };
+
     nixpkgs = {
       config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
         "pine64-pinephone-firmware"
